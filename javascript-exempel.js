a9db31765d4aede5d00d614f492fc71b9c2f44c4
Shmouni <72020321+Shmouni@users.noreply.github.com>
2021-03-22 14:37:25 +0100

    .

---
+++ javascript-exempel.js
@@ -0,0 +1,8 @@
+function authorDialog(){
+   alert("Shmouni har skapat sidan den 22/3/2021") 
+}
+function färg(){
+    document.getElementById("demo").style.fontSize = "25px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "yellow";        
+  }
